3b43d3c49976f1f9b22b6bef9de5284fa349db31
ykk <ykk@live.cn>
2019-12-09 09:42:44 +0800

    init module

---
+++ libwebsocket/src/main/cpp/websocket-lib.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <jni.h>
+
+#include <android/log.h>
+
+#define LOGGING_TAG "websocket"
+#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, LOGGING_TAG, __VA_ARGS__)
+
+
+
+
+
---
+++ libwebsocket/src/main/java/com/androidboot/websocket/Websocket.java
@@ -0,0 +1,8 @@
+package com.androidboot.websocket;
+
+public class Websocket {
+
+    static {
+        System.loadLibrary("websocket");
+    }
+}
